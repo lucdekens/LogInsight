1337674305c068c03052c6b55b61e0f2d9579f24
lucdekens <dekens.luc@gmail.com>
2016-04-29 16:37:21 +0200

    Added files via upload

---
+++ Examples/Query-Events.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module -Name LogInsight
+
+$sConnect = @{
+    Server = 'LogInsight.local.lab'
+    User = 'admin'
+    Password = 'VMware1!'
+}
+
+Connect-LogInsight @sConnect
+
+$constraint = Get-LogInsightConstraint -Field 'hostname' -Operator STARTS_WITH -Value 'esx1'
+
+$result = Get-LogInsightEvent -Constraint $constraint -Limit 100 -Event
+$result.events
