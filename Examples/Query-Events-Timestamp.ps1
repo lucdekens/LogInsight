24559553d7224123e2122417fa93b98cdea14953
LucD <dekens.luc@gmail.com>
2016-04-30 20:38:42 +0200

    v1.0
    Added Help
    DateTime constraint support added

---
+++ Examples/Query-Events-Timestamp.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module -Name LogInsight
+
+$sConnect = @{
+    Server = 'LogInsight.local.lab'
+    User = 'admin'
+    Password = 'VMware1!'
+}
+
+Connect-LogInsight @sConnect
+
+$constraint = Get-LogInsightConstraint -Field 'timestamp' -Operator GE -Value (Get-Date).AddHours(-1)
+
+$result = Get-LogInsightEvent -Constraint $constraint -Event
+$result.events
